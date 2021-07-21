09a238fa10b45f645f6111efbc660468e274929b
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2021-07-21 16:20:38 +0530

    syntax highliting, skip optimizations in dev mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 const markdownIt = require('markdown-it');
 const minify = require('html-minifier-terser').minify;
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass');
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/js');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   let options = {
     html: true
@@ -14,27 +17,37 @@ module.exports = function (eleventyConfig) {
   let markdownLib = markdownIt(options);
   eleventyConfig.setLibrary('md', markdownLib);
 
-  eleventyConfig.addPlugin(lazyImagesPlugin, {
-    imgSelector: '.project__img img',
-    preferNativeLazyLoad: false,
-    cacheFile: ''
+  eleventyConfig.addFilter('humanDate', function (dateStr) {
+    let date = new Date(dateStr);
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
   });
 
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-    if (outputPath && outputPath.endsWith('.html')) {
-      let minified = minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyJS: {
-          compress: true,
-          mangle: true
-        }
-      });
-      return minified;
-    }
-    return content;
-  });
+  if (process.env.ELEVENTY_ENV === 'production') {
+    eleventyConfig.addPlugin(lazyImagesPlugin, {
+      imgSelector: '.project__img img',
+      preferNativeLazyLoad: false,
+      cacheFile: ''
+    });
+  }
+
+  if (process.env.ELEVENTY_ENV === 'production') {
+    eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+      if (outputPath && outputPath.endsWith('.html')) {
+        let minified = minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+          minifyJS: {
+            compress: true,
+            mangle: true
+          }
+        });
+        return minified;
+      }
+      return content;
+    });
+  }
 
   return {
     dir: {
