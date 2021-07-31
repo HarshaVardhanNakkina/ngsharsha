0a91b1f0154e3a848e7da04da6f545bd6e665061
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2021-07-31 14:04:37 +0530

    global metadata put into a file

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Hi, fellow developer 👋 I'm HarshaVardhan",
+  description:
+    'Welcome to my blog. This is where I share my knowledge💡, showcase my projects🛠️, talk about my learnings📙, and more. I am a web developer🕷️, programming enthusiast💻🖱️.',
+  keywords: 'HarshaVardhan, Ganeshh__, NGS HarshaVardhan, HarshaVardhanNakkina',
+  author: 'HarshaVardhanNakkina'
+};
